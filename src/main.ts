cbc231c8826e61a3f3506602618bfbfbdffdc086
Hiro <laciferin@gmail.com>
2023-07-10 06:13:05 +0530

    add: config port
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 14 seconds

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = app.get<ConfigService>(ConfigService).get("port")
+  console.log(`App is running on ${port}`)
+  await app.listen(port)
 }
 bootstrap();
