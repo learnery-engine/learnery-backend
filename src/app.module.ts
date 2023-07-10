17a96568e73b429060b659f53322450e017e85bd
Hiro <laciferin@gmail.com>
2023-07-10 08:29:11 +0530

    import: auth module
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 19 seconds

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from './auth/auth.module';
 import * as Joi from "joi";
 
 @Module({
@@ -15,6 +16,7 @@ import * as Joi from "joi";
       PORT: Joi.number().default(1606),
     }),
   }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
