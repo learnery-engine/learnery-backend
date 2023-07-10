3dcc3ec504c64c63eaaa1498b504bb4e5deec78b
Hiro <laciferin@gmail.com>
2023-07-10 07:06:36 +0530

    e2e::
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 9 minutes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,42 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from "@nestjs/testing"
+import { AppModule } from "../src/app.module"
+import { HttpStatus, INestApplication, ValidationPipe } from "@nestjs/common"
+import * as pactum from "pactum"
+import { ConfigService } from "@nestjs/config"
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe("App e2e", () => {
+  let app: INestApplication
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  let url: string
+  beforeAll(async () => {
+    const ModuleRef = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    }).compile()
+    app = ModuleRef.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+    await app.init()
+    const port = app.get(ConfigService).get("PORT")
+    url = `http://localhost:${port}`
+    await app.listen(port)
+    pactum.request.setBaseUrl(url)
+  })
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+  afterAll(async () => {
+    await app.close()
+  })
+
+  describe("Auth", function () {
+    describe("Sign up", () => {
+      it.todo("sign up the user") //TODO:
+    })
+    describe("Sign in", () => {
+      it.todo("sign the user") //TODO:
+    })
+  })
+
+  describe("Course",()=>{
+    it.todo("CRUD Courses") //TODO:
+  })
+
+})
