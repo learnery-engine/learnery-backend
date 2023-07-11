069a4a70c89dedf29583472439fd0cdca15b0635
Hiro <laciferin@gmail.com>
2023-07-11 11:40:33 +0530

    disable: shutdown hooks owing to over resource consumption
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 1 minute

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
-  app.enableShutdownHooks()
+  // app.enableShutdownHooks()
 
   const port = app.get<ConfigService>(ConfigService).get('port') || 1606
   console.log(`App is running on ${port}`)
