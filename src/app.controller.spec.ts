71856fc96d076d4e270f619ff286673a80098748
Hiro <laciferin@gmail.com>
2023-07-11 09:16:36 +0530

    feat: health check
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 2 minutes

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -10,15 +10,16 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, PrismaService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return {message: "app is up and running"}', () => {
+      expect(appController.getHello()).toBe({message: "app is up and running"});
     });
+
   });
 });
