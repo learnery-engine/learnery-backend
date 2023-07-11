51329d44f39bef0078eeee873e938605daf1cb5a
Hiro <laciferin@gmail.com>
2023-07-11 11:39:32 +0530

    gracefully: exit db cont
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 13 seconds

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from "@nestjs/config";
-import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
-  const port = app.get<ConfigService>(ConfigService).get("port") || 1606
+  app.enableShutdownHooks()
+
+  const port = app.get<ConfigService>(ConfigService).get('port') || 1606
   console.log(`App is running on ${port}`)
   await app.listen(port)
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,35 @@
-import { Injectable } from "@nestjs/common"
-import { PrismaClient } from "@prisma/client"
-import { ConfigService } from "@nestjs/config"
+import { INestApplication, Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  // https://docs.nestjs.com/fundamentals/lifecycle-events
+
+  async onModuleInit() {
+    console.info(new Date(), 'connecting to the database')
+    await this.$connect()
+  }
+
+  async onModuleDestroy() {
+    console.warn(new Date(), 'disconnecting the database')
+    await this.$disconnect()
+    console.info(new Date(), 'disconnected database')
+  }
+  async enableShutdownHooks(app: INestApplication) {
+    console.warn('disconnecting the database')
+    this.$on('beforeExit', async () => {
+      this.$disconnect()
+      await app.close()
+    })
+  }
   constructor(config: ConfigService) {
-    const sslcert = "?sslcert=root.crt"
+    const sslcert = '?sslcert=root.crt'
     //https://www.prisma.io/docs/concepts/database-connectors/postgresql#configuring-an-ssl-connection
     super({
       datasources: {
         db: {
-          url: config.get<string>("DATABASE_URL") + sslcert,
+          url: config.get<string>('DATABASE_URL') + sslcert,
         },
       },
     })
