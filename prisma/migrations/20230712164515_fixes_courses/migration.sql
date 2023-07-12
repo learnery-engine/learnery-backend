63840327a680b6154db96791aaa8c23a9423c1a2
Hiro <laciferin@gmail.com>
2023-07-12 22:32:24 +0530

    migrations: remove
    Signed-off-by: Hiro <laciferin@gmail.com>
    
    Took 1 minute

--- prisma/migrations/20230712164515_fixes_courses/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "courses" ADD CONSTRAINT "courses_lesson_card_template_id_fkey" FOREIGN KEY ("lesson_card_template_id") REFERENCES "LessonCardTemplate"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
