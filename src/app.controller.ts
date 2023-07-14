9c907ae3518df69bc961b3b429af7b6e599bd4ca
Hiro <laciferin@gmail.com>
2023-07-14 14:11:34 +0530

    :zap: caching app controllers For
    Signed-off-by: Hiro <laciferin@gmail.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get, UseInterceptors } from '@nestjs/common'
 import { AppService } from './app.service'
-import { CacheInterceptor } from '@nestjs/cache-manager'
+import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager'
 
 @Controller()
+@UseInterceptors(CacheInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @UseInterceptors(CacheInterceptor)
+  @CacheKey('home')
+  @CacheTTL(Number.MAX_SAFE_INTEGER) //default is just 1 sec
   getHello() {
     return this.appService.getHello()
   }
 
+  @CacheTTL(5 * 1000)
   @Get('health')
   getHealth() {
     return this.appService.getHealth()
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,5 @@
-import { Injectable, UseInterceptors } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
-import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager'
-import { sleep } from '../utils'
 
 class ServiceStatus {
   constructor(public ok: boolean = true, public status: string = 'OK', public debug: any = undefined) {}
@@ -16,9 +14,7 @@ export class AppService {
     return {message: "app is up and running"};
   }
 
-  @CacheKey('health')
-  @CacheTTL(3*1000)
-  @UseInterceptors(CacheInterceptor)
+
   async getHealth(){
     let dbStatus = new ServiceStatus()
 
@@ -32,7 +28,6 @@ export class AppService {
       dbStatus.status = "connection failed"
       dbStatus.debug = err
     })
-    await sleep(1000)
     return {
       db: dbStatus,
       app: new ServiceStatus(),
