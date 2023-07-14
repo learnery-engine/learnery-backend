1f8ab39107e2088128e606407f5dc4a7ce7c5c21
Hiro <laciferin@gmail.com>
2023-07-14 13:32:23 +0530

    feat: compression- compress responses exceeding 512 bytes
    Signed-off-by: Hiro <laciferin@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { ValidationPipe, VersioningType } from '@nestjs/common'
 import * as cookieParser from 'cookie-parser'
+import * as compression from 'compression'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -11,14 +12,21 @@ async function bootstrap() {
   const config = app.get<ConfigService>(ConfigService)
   // app.enableShutdownHooks()
 
-  const port = config.get('PORT') || 1606
-  console.log(`App is running on ${port}`)
   app.use(cookieParser(config.get('JWT_SECRET')))
   app.enableCors()
   app.enableVersioning({
     type: VersioningType.HEADER,
     header: 'Accept-Version',
   })
+
+  app.use(
+    compression({
+      threshold: 512, // only responses exceeding 512 bytes will be compressed
+    }),
+  )
+
+  const port = config.get('PORT') || 1606
+  console.log(`App is running on ${port}`)
 
   await app.listen(port)
 }
